3ea558daa2e93888731d38240e7f09d4358a02d4
Travis Busen <busen@me.com>
2023-03-14 05:49:40 -0500

    dynamic routing

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -3,5 +3,8 @@ from first_app import views
 
 # domain.com/first_app
 urlpatterns = [
-    path('', views.simple_view)
+    path('', views.simple_view),
+
+    path('<topic>/', views.news_view),
+    path('<int:num1>/<int:num2>', views.add_view)
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import HttpResponse
+
+
+articles = {
+    "sports": "Sports Page",
+    "finance": "Finance Page",
+    "politics": "Politics Page",
+}
+
 
 def simple_view(request):
     return HttpResponse("Simple View in First_App")
+
+
+#def sports_view(request):
+#    return HttpResponse(articles.get("sports"))
+
+
+#def finance_view(resquest):
+#    return HttpResponse(articles.get("finance"))
+
+def news_view(request, topic: str):
+    return HttpResponse(articles.get(topic))
+
+def add_view(request, num1: int, num2: int):
+    #domain.com/first_app/3/4 --> 7
+    result = num1 + num2
+    return HttpResponse((str(result)))
