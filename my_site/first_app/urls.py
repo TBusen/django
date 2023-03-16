fef740235a75fd2cc0ab0b4a09c4e9ed4f89a501
Travis Busen <busen@me.com>
2023-03-16 05:40:43 -0500

    connecting view to simple template

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 from first_app import views
 
-# domain.com/first_app
-urlpatterns = [
-    path('<int:num_page>', views.num_page_view),
-    path('<str:topic>/', views.news_view, name='topic-page')
-]
+
+urlpatterns = [path('', views.simple_view)]  # domain.com/first_app
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,29 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse, Http404, HttpResponseRedirect
-from django.urls import reverse
 
 
-
-articles = {
-    "sports": "Sports Page",
-    "finance": "Finance Page",
-    "politics": "Politics Page",
-}
-
-
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        raise Http404(("404 generic Error"))
-
-#domain.com/first_app/0 --> domain.com/first_app/sports
-
-def num_page_view(request, num_page):
-
-    topics_list = list(articles.keys())
-    topic = topics_list[num_page]
-    webpage = reverse('topic-page', args=[topic]) # refer to url name and pass in args
-    return HttpResponseRedirect(webpage)
-
+def simple_view(request):
+    return render(request, 'first_app/example.html')
