1a3cd6061920f2e036ea621b9299cae062081da0
Travis Busen <busen@me.com>
2023-03-18 07:04:43 -0500

    django filters

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -13,7 +13,7 @@ def example_view(request: str) -> HttpResponse:
 def variable_view(request: str) -> HttpResponse:
     # my_app/templates/my_app/variable.html
 
-    my_var = {'first_name': 'Rosalind', 'last_name': 'Franklin',
+    my_var = {'first_name': 'RosalInd', 'last_name': 'FrankLin',
               'some_list':[1,2,3], 'some_dict': {'inside_key': 'inside_value'}}
 
     return render(request, 'my_app/variable.html', context=my_var)
