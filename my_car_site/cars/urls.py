52111def17550f58598fa2064caf49e1d651ff66
Travis Busen <busen@me.com>
2023-03-21 07:47:28 -0500

    new cars site

---
+++ my_car_site/cars/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from cars import views
+
+app_name = 'cars'
+
+# domain.com/cars/delete
+
+urlpatterns = [
+    path('list/', views.list, name = 'list'),
+    path('add/' ,views.add, name = 'add'),
+    path('delete/' ,views.delete, name = 'delete'),
+               ]
