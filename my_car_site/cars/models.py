b60070203a619c10ff9fc7c9768e70330c38051b
Travis Busen <busen@me.com>
2023-03-22 06:06:22 -0500

    forms to db connection

--- my_car_site/cars/models.py
+++ my_car_site/cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    # pk
+    brand = models.CharField(max_length=30)
+    year = models.IntegerField()
+    color = models.CharField(max_length=20, default=None)
+
+    def __str__(self):
+        return f"Car is {self.brand} {self.year}"
--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    context = {'all_cars':all_cars}
+    return render(request, 'cars/list.html', context=context)
 
 def add(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand=request.POST.get('brand')
+        year=int(request.POST.get('year'))
+        color=request.POST.get('color')
+
+        models.Car.objects.create(brand=brand, year=year, color=color)
+        # if user submitted new car ---> list.html
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        # delete the car
+        pk = request.POST.get('pk')
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found')
+            return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/delete.html')
