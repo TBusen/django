378d7425a71afd5aac3fd12a2721e50c1b7add3f
Travis Busen <busen@me.com>
2023-03-23 05:46:01 -0500

    modify forms

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+
+
+
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('TIME INFORMATION', {'fields':['year']}),
+         ('CAR INFORMATION', {'fields': ['brand', 'color']})]
+
+
+
+
+admin.site.register(Car, CarAdmin)
--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -5,30 +5,32 @@ from . import models
 # Create your views here.
 def list(request):
     all_cars = models.Car.objects.all()
-    context = {'all_cars':all_cars}
-    return render(request, 'cars/list.html', context=context)
+    context = {"all_cars": all_cars}
+    return render(request, "cars/list.html", context=context)
+
 
 def add(request):
     if request.POST:
-        brand=request.POST.get('brand')
-        year=int(request.POST.get('year'))
-        color=request.POST.get('color')
+        brand = request.POST.get("brand")
+        year = int(request.POST.get("year"))
+        color = request.POST.get("color")
 
         models.Car.objects.create(brand=brand, year=year, color=color)
         # if user submitted new car ---> list.html
-        return redirect(reverse('cars:list'))
+        return redirect(reverse("cars:list"))
     else:
-        return render(request, 'cars/add.html')
+        return render(request, "cars/add.html")
+
 
 def delete(request):
     if request.POST:
         # delete the car
-        pk = request.POST.get('pk')
+        pk = request.POST.get("pk")
         try:
             models.Car.objects.get(pk=pk).delete()
-            return redirect(reverse('cars:list'))
+            return redirect(reverse("cars:list"))
         except:
-            print('pk not found')
-            return redirect(reverse('cars:list'))
+            print("pk not found")
+            return redirect(reverse("cars:list"))
     else:
-        return render(request, 'cars/delete.html')
+        return render(request, "cars/delete.html")
