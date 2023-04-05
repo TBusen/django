992965adb206c7f85866d647a1c925049a95634e
Travis Busen <busen@me.com>
2023-04-05 08:01:16 -0400

    new su added

--- mysite/cars/forms.py
+++ mysite/cars/forms.py
@@ -3,13 +3,14 @@ from .models import Review
 from django.forms import ModelForm
 
 # class ReviewForm(forms.Form):
-    
+
 #     first_name = forms.CharField(label='First Name', max_length=100)
 #     last_name = forms.CharField(label = 'Last Name', max_length=100)
 #     email = forms.EmailField(label='Email')
 #     review = forms.CharField(label='Please Write your Review here', widget=forms.Textarea(attrs={'class':'myform',
 
+
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = ['first_name', 'last_name', 'stars']
+        fields = ["first_name", "last_name", "stars"]
